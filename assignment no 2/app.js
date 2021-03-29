568ad4cf2bc699942afed594521b9a50a6130fe6
Abdul Samad <75996134+abdulsamad516@users.noreply.github.com>
2021-03-29 15:48:46 +0500

    Add files via upload

---
+++ assignment no 2/app.js
@@ -0,0 +1,61 @@
+//OVERWRITE VALUE WHILE MAKING NEW OBJECT
+
+
+// let obj1 = {
+//     name: "leeza adil",
+//     stuDetails: {
+//         lastName:"siddiqui",
+//         city: "karachi"
+//     }
+// }
+
+// let obj2 = {
+//     school: "KGS"
+// }
+
+
+// let newObj = Object.assign({},obj1,obj2)
+// obj1.stuDetails.city = "amsterdam"
+// console.log(newObj)
+// console.log(obj1,obj2)
+
+
+
+//===================================================ARROW FUNCTION
+
+// const sumArrow = (a,b) => {
+//     return a+b;
+// }
+// console.log(sumArrow(25,25))
+
+
+
+//====================================================FOR-IN-----FOR-OF LOOP===========
+// obj = {
+//     name: "samad",
+//     age: 18,
+//     hobbies: "programming"
+// }
+
+// for (const key in obj) {
+//     console.log(obj[key]);
+// }
+
+
+//======================FOR OF
+
+// friends = ['samad','hamza','ahsan','abdullah']
+
+// for (let name of friends) {
+//     console.log(name)
+// }
+
+
+
+//TENARY OPERATER
+
+// var age = 80;
+
+// var check = age > 30 && age < 50 ? "you can sit on ride" : "you are not allowed to sit";
+// console.log(check)
+
